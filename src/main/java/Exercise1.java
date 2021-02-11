7df0e9a9f73b3b1c687876d1d627fb51d2687205
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-02-12 00:22:27 +0100

    Initial commit. Add Ex.1

---
+++ src/main/java/Exercise1.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class Exercise1 {
+
+    public void findAndPrintValidNumbers(String filePath) {
+
+        try (FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader)){
+                String phoneNumber = reader.readLine();
+
+            while (phoneNumber != null){
+                if(phoneNumberValidation(phoneNumber)){
+                    System.out.println(phoneNumber);
+                    phoneNumber = reader.readLine();
+                }else {phoneNumber = reader.readLine();}
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public boolean phoneNumberValidation(String phoneNumber) {
+        boolean isValidNumber = false;
+        if(phoneNumber.length() == 14) {
+            if (Pattern.matches("^\\(\\d{3}\\)\\s\\d{3}\\-\\d{4}$", phoneNumber)) {
+                isValidNumber = true;
+            }
+        }else
+            if(phoneNumber.length() == 12) {
+                if(Pattern.matches("^\\d{3}\\-\\d{3}\\-\\d{4}$", phoneNumber)) {
+                    isValidNumber = true;
+                }
+            }
+        return isValidNumber;
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\vadym\\Desktop\\PhonenUmbers.txt";
+        new Exercise1().findAndPrintValidNumbers(filePath);
+    }
+
+}
